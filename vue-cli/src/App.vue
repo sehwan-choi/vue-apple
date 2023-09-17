f12955e22e1aa987e0c3d5662e33cdc8b6a12ce2
sehwan <nu940907@gmail.com>
2023-09-17 19:42:21 +0900

    vue create + 구조 이해

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <app-header 
+      v-bind:propsdata="str" 
+      v-on:renew="renewString"></app-header>
+      <app-header 
+        v-bind:propsdata="str2" 
+        v-on:keyOn="keyString"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      str: 'Header',
+      str2: 'No Text'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  }, 
+  methods: {
+    renewString: function(val) {
+      this.str = val;
+    },
+    keyString: function(val) {
+      this.str2 = val;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+    <button v-on:click="sendUpEvent">up</button>
+    <button v-on:click="sendDownEvent">down</button>
+    <input v-model="message" v-on:keypress="keyEvent">
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    data: function() {
+        return {
+            num: 10,
+            message: ''
+        }
+    },
+    methods: {
+        sendUpEvent: function() {
+            this.num++;
+            this.$emit('renew', this.num);
+        },
+        sendDownEvent: function () {
+            this.num--;
+            this.$emit('renew', this.num);
+        },
+        keyEvent: function() {
+            this.$emit('keyOn', this.message);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
